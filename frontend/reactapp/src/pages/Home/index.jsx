4f947d45c0c33412c7cb2787f3a08cc6fba3e2a4
Carlos Eduardo <74271104+carlosedu757@users.noreply.github.com>
2022-06-27 14:32:12 -0300

    Atualizações pontuais

--- frontend/reactapp/src/pages/Home/index.jsx
+++ frontend/reactapp/src/pages/Home/index.jsx
@@ -6,7 +6,7 @@ export function Home() {
   return (
     <div className="container">
       <img src={logo} alt="logo" />
-      <button type="button">Login</button>
+      <a href="../Login/index.jsx"><button type="button">Login</button></a>
       <button type="button">Criar Conta</button>
     </div>
   )
--- frontend/reactapp/src/pages/Login/index.jsx
+++ frontend/reactapp/src/pages/Login/index.jsx
@@ -6,8 +6,11 @@ export function Login() {
   return (
     <div className="container">
       <img src={logo} alt="logo" />
-      <button type="button">Login</button>
-      <button type="button">Criar Conta</button>
+      <h2>Login</h2>
+      <input type="text" placeholder="Digite seu nome de usuário..." />
+      <h2>Senha</h2>
+      <input type="text" placeholder="Digite sua senha..." />
+      <button type="button">Entrar</button>
     </div>
   )
 }
