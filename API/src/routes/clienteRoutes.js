58c682b939de97ac6b094f70a0ad361c9eebf975
Carlos Eduardo <74271104+carlosedu757@users.noreply.github.com>
2022-07-01 01:25:25 -0300

    Update clienteRoutes.js

--- API/src/routes/clienteRoutes.js
+++ API/src/routes/clienteRoutes.js
@@ -4,8 +4,8 @@ import loginRequired from '../middlewares/loginRequired';
 const router = new Router();
 
 router.post('/cadastro', clienteController.store);
-router.put('/atualizar/', loginRequired, clienteController.update);
-router.delete('/apagar/', loginRequired, clienteController.delete);
+router.put('/atualizar', loginRequired, clienteController.update);
+router.delete('/apagar', loginRequired, clienteController.delete);
 router.get('/index', clienteController.show);
 
 export default router;
